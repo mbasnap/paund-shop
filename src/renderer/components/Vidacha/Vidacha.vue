9f8e47847a934e9feac462287dde57ba67393e83
admin <you@example.com>
2018-09-11 17:40:45 +0300

    dddd

--- src/renderer/components/Vidacha/Vidacha.vue
+++ src/renderer/components/Vidacha/Vidacha.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="">
     <div class="col h-100">
-        <div class="row h-50 bg-info  " >
+        <div class="row bg-info  " >
           <div class="col-sm-5">Klient</div>        
           <div class="col-sm-3">Bilet</div>
           <div class="col-sm-4">Calculator</div>
         </div>
-        <div class="row h-50  bg-danger ">
+        <div class="row"></div>
+        <div class="row  bg-danger ">
           <div class="col  "> Obespechenie</div>
         </div>     
     </div>
