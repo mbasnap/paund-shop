39da2734cf877ddd73e4e24f68d585e5eab187bf
mbasnap <mbasnap@yandex.ua>
2018-09-14 12:21:46 +0300

    fix kassa files

--- src/renderer/components/Kassa/KassaList.vue
+++ src/renderer/components/Kassa/KassaList.vue
@@ -5,6 +5,11 @@
   <li class="list-group-item">7250</li>
   <li class="list-group-item">50000.00</li>
   <li class="list-group-item">40000.00</li>
+  <li class="list-group-item">235</li>
+  <li class="list-group-item">458</li>
+  <li class="list-group-item">7250</li>
+  <li class="list-group-item">50000.00</li>
+  <li class="list-group-item">40000.00</li>
 </ul>
 </template>
 
