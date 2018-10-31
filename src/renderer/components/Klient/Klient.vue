8a563a6cdf59b94816c2cdddcb787ff2ee300fb7
mbasnap <mbasnap@yandex.ua>
2018-10-31 12:27:35 +0300

    klient module

--- src/renderer/components/Klient/Klient.vue
+++ src/renderer/components/Klient/Klient.vue
@@ -1,16 +1,33 @@
 <template>
-    <div>
-        <h1>{{current.title}}</h1>
-        <p>{{isSaved}}</p>
-        <p>
-        <button @click="saveCurrent({logo: 'asdsad'})">save</button>
-        </p>
+<div class="mt-3">
+    <form>  
+    <div class="form-group">
+        <searcher id="family" placeholder="Family"></searcher>
     </div>
+        <div class="form-row">
+            <div class="form-group col-md-6">
+                <input type="text" class="form-control" id="name" placeholder="Email">
+            </div>
+            <div class="form-group col-md-6">
+                <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
+            </div>
+        </div>
+        <div class="form-group">
+            <button class="btn btn-primary">Cacsel</button>
+            <button  class="btn btn-primary">Submit</button>
+        </div>
+    </form>
+</div>
+
+
+
 </template>
 
 <script>
+import {Searcher} from "@/components/Widgets"
 import {mapGetters, mapActions} from 'vuex'
 export default {
+    components: {Searcher},
     computed: {...mapGetters('klient',['current', 'isSaved'])},
     methods: {...mapActions('klient',['saveCurrent'])}
 }
--- src/renderer/store/modules/settings.js
+++ src/renderer/store/modules/settings.js
@@ -4,23 +4,11 @@ const defaultMainPath = path.dirname (remote.app.getPath ('exe'))
 
 const state = {
     settings: {
-<<<<<<< HEAD
         logo: "Logo"
     }
 }
 const getters = {
     settings: state => state.settings
-=======
-        logoText: 'logo'
-    },
-    mainPath: null
-}
-const getters = {
-    getAll: (state) => {return state.settings},
-    mainPath: state => {
-        if (!state.mainPath) return defaultMainPath
-    }
->>>>>>> 8f6d850bb5da6bb591a6562ec5352c432dd78ebd
 }
 const mutations = {
     settings: (state, payload) => state.settings = payload
